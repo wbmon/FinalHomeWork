1857dcab5522e16b6fddd180951ef106b5b66e2f
Dmitry Molchanov <admin@nevinka-school1.ru>
2022-10-02 12:21:38 +0300

    A program has been written that solves the task.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(string[] array)
+{
+    for (int i=0; i < array.Length; i++) 
+    {
+        Console.Write($"Enter {i+1} string element: ");
+        array[i] = Console.ReadLine();
+    }
+}
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.ResetColor();
+    Console.WriteLine();
+}
+Console.Clear();
+Console.Write("Enter the dimension of the array: ");
+int dimens=Convert.ToInt32(Console.ReadLine());
+
+string[] array1 = new string[dimens];
+string[] array2 = new string[dimens];
+FillArray(array1);
+
+SecondArray(array1, array2);
+Console.Write($"A new array of strings whose length is less than or equal to 3 characters: "); 
+PrintArray(array2);
